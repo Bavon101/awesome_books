ef3f6c8441488c9aee231e18924dc219bcd25576
Bavon101 <bavonfate@gmail.com>
2022-02-07 17:43:40 +0300

    Hot fix: fix linting issues

--- js/main.js
+++ js/main.js
@@ -8,42 +8,43 @@ function saveBooks() {
 }
 function createBookElements() {
   booksSection.replaceChildren();
-	if (books.length > 0) {
+  if (books.length > 0) {
     const booksList = document.createElement('ul');
     booksList.style.listStyleType = 'none';
     booksList.style.margin = '0';
     booksList.style.padding = '0';
     books.map((b) => {
-    	console.log(b);
-    	const bookCard = document.createElement('li');
-    	const titleElement = document.createElement('h3');
-    	titleElement.style.padding = '0';
-    	titleElement.style.margin = '0';
-    	const titleText = document.createTextNode(b.title);
-    	titleElement.appendChild(titleText);
-    	bookCard.appendChild(titleElement);
-    	const authorElement = document.createElement('h4');
-    	authorElement.style.margin = '0';
-    	const authorText = document.createTextNode(b.author);
-    	authorElement.appendChild(authorText);
-    	bookCard.append(authorElement);
-    	const removeBtn = document.createElement('button');
-    	const btnText = document.createTextNode('Remove');
-    	removeBtn.onclick = function () {
-    		removeBook(b.id);
-    	};
-    	removeBtn.appendChild(btnText);
-    	bookCard.appendChild(removeBtn);
-    	booksList.appendChild(bookCard);
-    	const divider = document.createElement('hr');
-    	booksList.appendChild(divider);
+    console.log(b);
+    const bookCard = document.createElement('li');
+    const titleElement = document.createElement('h3');
+    titleElement.style.padding = '0';
+    titleElement.style.margin = '0';
+    const titleText = document.createTextNode(b.title);
+    titleElement.appendChild(titleText);
+    bookCard.appendChild(titleElement);
+    const authorElement = document.createElement('h4');
+    authorElement.style.margin = '0';
+    const authorText = document.createTextNode(b.author);
+    authorElement.appendChild(authorText);
+    bookCard.append(authorElement);
+    const removeBtn = document.createElement('button');
+    const btnText = document.createTextNode('Remove');
+    removeBtn.onclick = function () {
+    	removeBook(b.id);
+    };
+    removeBtn.appendChild(btnText);
+    bookCard.appendChild(removeBtn);
+    booksList.appendChild(bookCard);
+    const divider = document.createElement('hr');
+    booksList.appendChild(divider);
+    return booksList;
     });
     booksSection.appendChild(booksList);
   } else {
     const noBooks = document.createElement('h3');
     const noBooksText = document.createTextNode('There are no books available');
     noBooks.appendChild(noBooksText);
-		booksSection.appendChild(noBooks);
+    booksSection.appendChild(noBooks);
   }
 }
 
