8da9bb96b5ed7508cc7f140cd21bc812a86f2727
Bavon101 <bavonfate@gmail.com>
2022-02-08 15:50:56 +0300

    Add books class with defined methods

---
+++ js/books.js
@@ -0,0 +1,28 @@
+class AwesomeBooks {
+	books = [];
+	constructor () {
+		this.books
+	}
+	saveBooks() {
+      const booksList = JSON.stringify(this.books);
+      localStorage.setItem('books', booksList);
+	}
+	addBook(title, author) {
+      const book = {
+      	title,
+      	author,
+      	id: Date.now(),
+      };
+      this.books.unshift(book);
+      this.saveBooks();
+	}
+
+	removeBook(id) {
+      this.books = this.books.filter((a) => a.id !== id);
+      this.saveBooks();
+	}
+
+	getStoredBooks() {
+      this.books = JSON.parse(localStorage.getItem('books'));
+	}
+}
