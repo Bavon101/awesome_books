165d9119996b8eab269cb1dcced6fd4f58a7687e
Bavon101 <bavonfate@gmail.com>
2022-02-08 17:25:32 +0300

    Hot fix linter errors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+class AwesomeBooks {
+  books = [];
+  constructor() {
+    this.books
+  }
+  saveBooks() {
+    const booksList = JSON.stringify(this.books);
+    localStorage.setItem('books', booksList);
+  }
+  addBook(title, author) {
+    const book = {
+      title,
+      author,
+      id: Date.now(),
+    };
+    this.books.unshift(book);
+    this.saveBooks();
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter((a) => a.id !== id);
+    this.saveBooks();
+  }
+
+  getStoredBooks() {
+    this.books = JSON.parse(localStorage.getItem('books'));
+  }
+}
 const books = new AwesomeBooks();
 const booksSection = document.getElementById('books');
 const form = document.getElementById('book_form');
